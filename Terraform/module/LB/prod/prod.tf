fe675d321874fe6a51eb1011c8ae80c0106a30f5
aida <ghk01214@naver.com>
2024-12-26 23:19:03 +0900

    메트릭 감시 시스템 추가
    프로메테우스+그라파나

--- Terraform/module/LB/prod/prod.tf
+++ Terraform/module/LB/prod/prod.tf
@@ -20,7 +20,7 @@ resource "aws_lb_listener_rule" "ingress" {
 
 resource "aws_lb_target_group" "ingress" {
   name     = "gitfolio-ingress-tg"
-  port     = 31258 // LB가 타겟으로 트래픽을 전달하는 포트(ALB->target(instance))
+  port     = var.target_port["NodePort"] + var.target_port["http"] // LB가 타겟으로 트래픽을 전달하는 포트(ALB->target(instance))
   protocol = var.target_protocol
   vpc_id   = var.vpc_id
 
@@ -48,19 +48,41 @@ resource "aws_lb_target_group_attachment" "ingress" {
 
 // ============================================================================================================
 
-resource "aws_lb_listener_rule" "master" {
+# resource "aws_lb_listener_rule" "oauth2" {
+#   listener_arn = var.listener_arn
+#   priority     = 2
+
+#   action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.ingress.arn
+#   }
+
+#   condition {
+#     path_pattern {
+#       values = ["/oauth2/*", "/login/*"]
+#     }
+#   }
+
+#   condition {
+#     host_header {
+#       values = ["www.gitfolio.site"]
+#     }
+#   }
+
+#   tags = {
+#     Name = "Gitfolio oauth2 routing"
+#   }
+# }
+
+// ============================================================================================================
+
+resource "aws_lb_listener_rule" "api" {
   listener_arn = var.listener_arn
-  priority     = 2
+  priority     = 3
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.master.arn
-  }
-
-  condition {
-    path_pattern {
-      values = ["/master", "/master/*"]
-    }
+    target_group_arn = aws_lb_target_group.ingress.arn
   }
 
   condition {
@@ -70,34 +92,28 @@ resource "aws_lb_listener_rule" "master" {
   }
 
   tags = {
-    Name = "Gitfolio k8s master routing"
+    Name = "Gitfolio k8s api routing"
   }
 }
 
-resource "aws_lb_target_group" "master" {
-  name     = "gitfolio-master-tg"
-  port     = var.target_port["k8s"] // LB가 타겟으로 트래픽을 전달하는 포트(ALB->target(instance))
-  protocol = "HTTPS"
-  vpc_id   = var.vpc_id
+// ============================================================================================================
 
-  health_check {
-    enabled             = true
-    healthy_threshold   = var.health_threshold
-    interval            = var.health_interval
-    matcher             = var.health_matcher
-    path                = "/master/healthz"
-    port                = var.health_port
-    protocol            = "HTTPS"
-    timeout             = var.health_timeout
-    unhealthy_threshold = var.health_unthreshold
+resource "aws_lb_listener_rule" "grafana" {
+  listener_arn = var.listener_arn
+  priority     = 4
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ingress.arn
+  }
+
+  condition {
+    host_header {
+      values = ["grafana.gitfolio.site"]
+    }
   }
 
   tags = {
-    Name = "Gitfolio k8s master target group"
+    Name = "Gitfolio k8s grafana routing"
   }
-}
-
-resource "aws_lb_target_group_attachment" "master" {
-  target_group_arn = aws_lb_target_group.master.arn
-  target_id        = var.master_id
 }
--- Terraform/module/Route53/resource.tf
+++ Terraform/module/Route53/resource.tf
@@ -3,9 +3,9 @@ resource "aws_route53_record" "gitfolio" {
   zone_id = data.aws_route53_zone.gitfolio.zone_id
   name    = format("www.%s", substr(var.route53_domain, 2, length(var.route53_domain) - 2))
   type    = "A"
-  
+
   alias {
-    name                   = substr(var.alb_dns_name, 0, length(var.alb_dns_name))
+    name = substr(var.alb_dns_name, 0, length(var.alb_dns_name))
     #name                   = substr(var.alb_dns_name, 0, length(var.alb_dns_name) - 1)
     zone_id                = var.alb_zone_id
     evaluate_target_health = true
@@ -17,7 +17,7 @@ resource "aws_route53_record" "gitfolio_api" {
   zone_id = data.aws_route53_zone.gitfolio.zone_id
   name    = format("api.%s", substr(var.route53_domain, 2, length(var.route53_domain) - 2))
   type    = "A"
-  
+
   alias {
     name                   = substr(var.alb_dns_name, 0, length(var.alb_dns_name))
     zone_id                = var.alb_zone_id
@@ -25,12 +25,27 @@ resource "aws_route53_record" "gitfolio_api" {
   }
 }
 
+resource "aws_route53_record" "gitfolio_grafana" {
+  count   = terraform.workspace == "prod" ? 1 : 0
+  zone_id = data.aws_route53_zone.gitfolio.zone_id
+  name    = format("grafana.%s", substr(var.route53_domain, 2, length(var.route53_domain) - 2))
+  type    = "A"
+
+  alias {
+    name                   = substr(var.alb_dns_name, 0, length(var.alb_dns_name))
+    zone_id                = var.alb_zone_id
+    evaluate_target_health = true
+  }
+}
+
+// ============================================================================================================
+
 resource "aws_route53_record" "gitfolio_dev" {
   count   = terraform.workspace == "dev" ? 1 : 0
   zone_id = data.aws_route53_zone.gitfolio.zone_id
   name    = format("dev.%s", substr(var.route53_domain, 2, length(var.route53_domain) - 2))
   type    = "A"
-  
+
   alias {
     name                   = substr(var.alb_dns_name, 0, length(var.alb_dns_name))
     zone_id                = var.alb_zone_id
@@ -39,7 +54,7 @@ resource "aws_route53_record" "gitfolio_dev" {
 }
 
 resource "aws_route53_record" "jenkins" {
-  count  = terraform.workspace == "feature-cicd" ? 1 : 0
+  count   = terraform.workspace == "feature-cicd" ? 1 : 0
   zone_id = data.aws_route53_zone.gitfolio.zone_id
   name    = format("jenkins.%s", substr(var.route53_domain, 2, length(var.route53_domain) - 2))
   type    = "A"
