abe0782ce9adc25aabfc64431cd6ec2e08ad0e3e
aida <ghk01214@naver.com>
2024-12-26 23:19:30 +0900

    쿠버네티스 클러스터 리소스 설정

---
+++ kubernetes/backend/service/service.sh
@@ -0,0 +1,7 @@
+kubectl apply -f auth-service.yaml
+kubectl apply -f kafka-service.yaml
+kubectl apply -f member-service.yaml
+kubectl apply -f notification-service.yaml
+kubectl apply -f payment-service.yaml
+kubectl apply -f resume-service.yaml
+kubectl apply -f zookeeper-service.yaml
